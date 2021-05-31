3c244d9eafa93ada14a6f408bdb7e5b42b76d166
Paweł Wójcik <pawel.jakub.wojcik@gmail.com>
2021-06-01 01:37:27 +0200

    fixed issue #2
    just added scrolling to top on animation end

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useRef } from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 import styled from "styled-components/macro"
@@ -6,7 +6,6 @@ import { AnimatePresence, motion } from "framer-motion"
 
 import { GlobalStyles } from "./globalStyles"
 import Header from "./header"
-import BackgroundThing from "./backgroundThing"
 export default function Layout({ children, location }) {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
@@ -18,10 +17,12 @@ export default function Layout({ children, location }) {
     }
   `)
 
+  const scrollable = useRef(null)
+
   return (
     <>
       <GlobalStyles />
-      <Container>
+      <Container ref={scrollable}>
         <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
         <AnimatePresence exitBeforeEnter initial={false}>
           <Main
@@ -29,6 +30,14 @@ export default function Layout({ children, location }) {
             initial="initial"
             animate="animate"
             exit="exit"
+            onAnimationComplete={() => {
+              if (scrollable.current)
+                scrollable.current.scrollTo({
+                  top: 0,
+                  left: 0,
+                  behavior: "auto",
+                })
+            }}
           >
             {children}
           </Main>
@@ -36,7 +45,6 @@ export default function Layout({ children, location }) {
 
         {/* <BackgroundThing /> */}
       </Container>
-      
     </>
   )
 }
@@ -59,7 +67,7 @@ const Container = styled.div`
   scrollbar-width: thin;
   scrollbar-color: #8c7759c2 #ffeedc; /* scroll thumb & track */
   &::-webkit-scrollbar {
-    width: 12px;
+    width: 8px;
   }
   &::-webkit-scrollbar-track {
     background: #ffeedc;
@@ -68,6 +76,7 @@ const Container = styled.div`
     background-color: #8c7759c2;
     border-radius: 20px;
     width: 12px;
+    opacity: 0.5;
   }
 
   @media (max-width: 600px) {
@@ -81,6 +90,5 @@ const Main = styled(motion.main)`
 `
 
 const Footer = styled.footer`
-  
   bottom: 0;
 `
